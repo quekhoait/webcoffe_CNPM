f06c807abf988d8801c68a4718c041cdc544a9f8
NTL-HUY <161469218+NTL-HUY@users.noreply.github.com>
2025-11-23 08:29:40 +0700

    change

--- eapp/controllers/index.py
+++ eapp/controllers/index.py
@@ -1,4 +1,5 @@
 from flask import render_template
 
 def home():
-    return render_template('page/home.html', )
+    # return render_template('page/home.html', )
+    return render_template('staff/staff.html')
