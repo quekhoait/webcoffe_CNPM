5bce35813ed28aba492fba5d003e3e2131660ea9
NTL-HUY <161469218+NTL-HUY@users.noreply.github.com>
2025-11-29 11:53:08 +0700

    add to order process

--- eapp/controllers/StaffController.py
+++ eapp/controllers/StaffController.py
@@ -10,7 +10,9 @@ from eapp.dao import DishCategoryDAO, DishDAO
 def load_staff():
     category = DishCategoryDAO.list()
     dishes = DishDAO.list()
-    return render_template('/staff/staff.html', category=category,dishes=dishes)
+    order = session.get('order', {})
+    total = sum(item['quantity'] * item['price'] for item in order.values())
+    return render_template('/staff/staff.html', category=category,dishes=dishes, total=total)
 
 
 def addToOrder():
@@ -19,32 +21,38 @@ def addToOrder():
             id
             name
             price
-            quantity
+            bonus_quantity
     """
     order = session.get('order',{})
     data = request.json
     id = str(data.get('id'))
-    is_change_quantity = False
+    # is_change_quantity = False
+    bonus_quantity = int(data.get('bonus_quantity',1))
 
     if id in order:
-        order[id]['quantity'] += 1
-        is_change_quantity = True
+        order[id]['quantity'] += bonus_quantity
+        # is_change_quantity = True
+        print("OKK bro")
+
     else:
         order[id] = {
             "id": id,
             "name": data.get('name'),
             "price": data.get('price'),
-            "quantity": 1
+            "quantity": bonus_quantity
         }
 
     session['order'] = order
 
- 
+    item_html = render_template('staff/order_item.html',dish=order[id])
+    total_price = sum(item['quantity']*item['price'] for item in order.values())
     return jsonify({
         "succes" : True,
-        "item" : order[id],
-        "total_quantity" : sum(i['quantity'] for i in order.values()),
-        "is_change_quantity" : is_change_quantity
+        "item_html" : item_html,
+        # "total_quantity" : sum(i['quantity'] for i in order.values()),
+        "total_price" : total_price,
+        'item' : order[id]
+        # "is_change_quantity" : is_change_quantity
     })
     
 
