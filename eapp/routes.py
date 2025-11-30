51c1fda8d2ec2a79c032d8d77d42e6e709872fb3
NTL-HUY <161469218+NTL-HUY@users.noreply.github.com>
2025-12-01 00:20:08 +0700

    cashier UI

--- eapp/routes.py
+++ eapp/routes.py
@@ -4,12 +4,15 @@ from flask import Flask, render_template
 # Thay đổi import tương đối thành import tuyệt đối:
 # from eapp.dao.Product import get_product
 from eapp import app
-from eapp.controllers import ProductController, StaffController, index
+from eapp.controllers import CashierController, ProductController, StaffController, index
 
 
 app.add_url_rule('/about-us','about-us',index.aboutUs)
 # app.add_url_rule('/','index',index.loadHome)
-app.add_url_rule('/','index',StaffController.load_staff)
+# demo
+app.add_url_rule('/staff','index2',StaffController.load_staff)
+app.add_url_rule('/','index',CashierController.home)
+
 
 
 app.add_url_rule('/api/dish','dish',ProductController.list)
@@ -17,4 +20,6 @@ app.add_url_rule('/api/dish','dish',ProductController.list)
 app.add_url_rule('/staff','staff',StaffController.load_staff)
 app.add_url_rule('/api/order','add_order',StaffController.addToOrder, methods=['post'])
 app.add_url_rule('/api/invoice','create_invoice',StaffController.create_invoice, methods=['post'])
+
+#cashier
 
